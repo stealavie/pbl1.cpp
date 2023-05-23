8ced81216513d649a00bd6407ced7da37df65470
stealavie <118667726+stealavie@users.noreply.github.com>
2023-05-23 09:54:14 +0700

    Update c1.cpp

--- c1.cpp
+++ c1.cpp
@@ -1,15 +1,13 @@
 #include <stdio.h>
+#include<stdlib.h>
 #include <limits.h>
-#define M 10
-#define N 10
-
 int min(int a, int b) {
     return (a < b) ? a : b;
 }
 
-void leastCost(int cost[M][N], int supply[M], int demand[N], int m, int n) {
+void leastCost(int **cost,int *supply,int *demand, int m, int n) {
     int i, j, minCost = 0;
-    int alloc[M][N] = {0};
+    int alloc[m][n] = {0};
     printf("DUONG DI TOI UU SU DUNG PHUONG PHAP CUOC PHI NHO NHAT:\n");
     while (1) {
         int minVal = INT_MAX;
@@ -33,27 +31,78 @@ void leastCost(int cost[M][N], int supply[M], int demand[N], int m, int n) {
         minCost += alloc[minRow][minCol] * cost[minRow][minCol];
          printf("x[%d][%d]=%d\n",minRow+1,minCol+1, alloc[minRow][minCol]);
     }
-
-//    printf("DUONG DI TOI UU SU DUNG PHUONG PHAP CUOC PHI NHO NHAT:\n");
-//    for (i = 0; i < m; i++) {
-//        for (j = 0; j < n; j++) {
-//            printf("%d ", alloc[i][j]);
-//        }
-//        printf("\n");
-//    }
-
+     for (int i=0; i<m; i++)
+        free(cost[i]);
+    free(cost);
+    free(supply);
+    free(demand);
     printf("CUOC PHI NHO NHAT LA: %d\n", minCost);
 }
-//O(m*n*(m+n-1))
-int main() {
-    int cost[M][N] = {{19, 30, 50, 10},
-                      {70, 30, 40, 60},
-                      {40, 8, 70, 20}};
-    int supply[M] = {7, 9, 18};
-    int demand[N] = {5, 8, 7, 14};
-    
 
-    leastCost(cost, supply, demand, 3, 4);
+int main() 
+{
+        while(1)
+		{
+        	printf ("-----------------------------------\n");
+        	printf ("1.Doc tu file\n");
+        	printf ("2.Nhap tu ban phim\n");
+        	printf("nhap lua chon cua ban\n");
+        	printf ("-----------------------------------\n");
+        	int lc;scanf ("%d",&lc);
+        	if (lc==2)
+			{
+        	printf ("NHAP SO DIEM CUNG VA CAU:\n");
+			int m,n;scanf ("%d%d",&m,&n);
+        	printf ("nhap ma tran cuoc phi\n");
+        	int **cost;
+	        cost=(int **)malloc(m * sizeof(int *));
+            for (int i=0; i<m; i++)
+               cost[i] = (int *)malloc(n * sizeof(int));
+            for (int i = 0; i <  m; i++)
+		 {
+                for (int j = 0; j < n; j++)
+                   scanf ("%d",&cost[i][j]);
+    }  
+            printf ("nhap so luong cung");
+            int *supply;
+            supply = (int *)malloc(m * sizeof(int));
+            for (int i=0; i<m; i++) scanf("%d",&supply[i]);
+            printf ("nhap so luong cau"); 
+			int *demand;
+            demand = (int *)malloc(n * sizeof(int));
+            for (int i=0; i<n; i++) scanf("%d",&demand[i]);
+			leastCost(cost,supply,demand,m,n);
+		}
+		else if (lc==1){
+	    	//  char filename[100];
+//    printf("Nhap ten tep tin: ");
+//    scanf("%s", filename);
 
+    FILE *file = fopen("D:/New folder/in.in", "r");//thay doi file 
+//     if (file == NULL)
+//     {
+//         printf("Khong the mo tep tin %s\n", filename);
+//         exit(1);
+//     }
+
+      int  m,n;
+    fscanf(file,"%d%d",&m,&n);
+     int **cost;
+	        cost=(int **)malloc(m * sizeof(int *));
+            for (int i=0; i<m; i++)
+               cost[i] = (int *)malloc(n * sizeof(int));
+        for (int i=0;i<m;i++)
+        {
+        	for (int j=0;j<n;j++) fscanf (file,"%d",&cost[i][j]);
+		}
+		 int *supply;
+            supply = (int *)malloc(m * sizeof(int));
+		for (int i=0;i<m;i++) fscanf (file,"%d",&supply[i]);
+		 int *demand;
+            demand = (int *)malloc(n * sizeof(int));
+		for (int j=0;j<n;j++) fscanf (file,"%d",&demand[j]);
+		leastCost(cost,supply,demand,m,n);
+	} else if (lc==0) break;
+	}
     return 0;
 }
